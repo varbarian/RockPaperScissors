8217a35500000416e4bf618597d41d070305af1e
vd-varbanov <varban.d.varbanov@gmail.com>
2021-08-02 16:29:33 +0300

    added button functionality to the rps project

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const buttons = document.querySelectorAll('button');
+const resultBoard = document.querySelector('p');
+const scoreBoard = document.querySelectorAll('p')[1];
+const statusMsg = document.querySelectorAll('p')[2];
+
 function computerPlay() {
     //Defining options of a RPS game.
     const gameOptions = ['rock', 'paper', 'scissors'];
@@ -7,67 +12,76 @@ function computerPlay() {
     return gameOptions[Math.floor(Math.random() * gameOptions.length)];
 }
 
+function playRound(playerSelection, computerSelection) {
+    //Make your function’s playerSelection parameter case-insensitive
+    const playerSelectionFiltered = playerSelection.toLowerCase();
+    //console.log(computerSelection);
+    if (playerSelectionFiltered === 'rock' && computerSelection === 'scissors') {
+        resultBoard.textContent = `You win! Rock beats scissors!`;
+        playerScore++;
+        scoreBoard.textContent = `Player Score: ${playerScore} Computer Score: ${computerScore} Draw: ${draw}`;
+        return playerScore;
+    } else if (playerSelectionFiltered === 'rock' && computerSelection === 'paper') {
+        resultBoard.textContent = 'You lose! Paper beats rock!';
+        computerScore++;
+        scoreBoard.textContent = `Player Score: ${playerScore} Computer Score: ${computerScore} Draw: ${draw}`;
+        return computerScore;
+    } else if (playerSelectionFiltered === 'rock' && computerSelection === 'rock') {
+        resultBoard.textContent = `Rock vs Rock! It's a tie!`;
+        draw++;
+        scoreBoard.textContent = `Player Score: ${playerScore} Computer Score: ${computerScore} Draw: ${draw}`;
+        return draw;
+    } else if (playerSelectionFiltered === 'scissors' && computerSelection === 'paper') {
+        resultBoard.textContent = 'You win! Scissors beats paper!';
+        playerScore++;
+        scoreBoard.textContent = `Player Score: ${playerScore} Computer Score: ${computerScore} Draw: ${draw}`;
+        return playerScore;
+    } else if (playerSelectionFiltered === 'scissors' && computerSelection === 'rock') {
+        resultBoard.textContent = 'You lose! Rock beats Scissors!';
+        computerScore++;
+        scoreBoard.textContent = `Player Score: ${playerScore} Computer Score: ${computerScore} Draw: ${draw}`;
+        return computerScore;
+    } else if (playerSelectionFiltered === 'scissors' && computerSelection === 'scissors') {
+        resultBoard.textContent = `Scissors vs Scissors! It's a tie!`;
+        draw++;
+        scoreBoard.textContent = `Player Score: ${playerScore} Computer Score: ${computerScore} Draw: ${draw}`;
+        return draw;
+    } else if (playerSelectionFiltered === 'paper' && computerSelection === 'rock') {
+        resultBoard.textContent = 'You win! Paper beats rock!';
+        playerScore++;
+        scoreBoard.textContent = `Player Score: ${playerScore} Computer Score: ${computerScore} Draw: ${draw}`;
+        return playerScore;
+    } else if (playerSelectionFiltered === 'paper' && computerSelection === 'scissors') {
+        resultBoard.textContent = 'You lose! Scissors beats paper!';
+        computerScore++;
+        scoreBoard.textContent = `Player Score: ${playerScore} Computer Score: ${computerScore} Draw: ${draw}`;
+        return computerScore;
+    } else if (playerSelectionFiltered === 'paper' && computerSelection === 'paper') {
+        resultBoard.textContent = `Paper vs Paper! It's a tie!`;
+        draw++;
+        scoreBoard.textContent = `Player Score: ${playerScore} Computer Score: ${computerScore} Draw: ${draw}`;
+        return draw;
+    } else {
+        resultBoard.textContent = `something is wrong.`;
+        scoreBoard.textContent = `Player Score: ${playerScore} Computer Score: ${computerScore} Draw: ${draw}`;
+        }
+    };
+
 let playerScore = 0;
 let computerScore = 0;
 let draw = 0;
 
-function game() {
-    for (let round = 0; round < 5; round ++) {
-    let computerSelection = computerPlay();
-    let playerSelection = prompt(`What do you pick?`);
-    function playRound(playerSelection, computerSelection) {
-        //Make your function’s playerSelection parameter case-insensitive
-        const playerSelectionFiltered = playerSelection.toLowerCase();
-        //console.log(computerSelection);
-        if (playerSelectionFiltered === 'rock' && computerSelection === 'scissors') {
-            console.log(`You win! Rock beats scissors!`);
-            playerScore++;
-            return playerScore;
-        } else if (playerSelectionFiltered === 'rock' && computerSelection === 'paper') {
-            console.log('You lose! Paper beats rock!');
-            computerScore++;
-            return computerScore;
-        } else if (playerSelectionFiltered === 'rock' && computerSelection === 'rock') {
-            console.log(`Rock vs Rock! It's a tie!`);
-            draw++;
-            return draw;
-        } else if (playerSelectionFiltered === 'scissors' && computerSelection === 'paper') {
-            console.log('You win! Scissors beats paper!');
-            playerScore++;
-            return playerScore;
-        } else if (playerSelectionFiltered === 'scissors' && computerSelection === 'rock') {
-            console.log('You lose! Rock beats Scissors!');
-            computerScore++;
-            return computerScore;
-        } else if (playerSelectionFiltered === 'scissors' && computerSelection === 'scissors') {
-            console.log(`Scissors vs Scissors! It's a tie!`);
-            draw++;
-            return draw;
-        } else if (playerSelectionFiltered === 'paper' && computerSelection === 'rock') {
-            console.log('You win! Paper beats rock!');
-            playerScore++
-            return playerScore;
-        } else if (playerSelectionFiltered === 'paper' && computerSelection === 'scissors') {
-            console.log('You lose! Scissors beats paper!');
-            computerScore++;
-            return computerScore;
-        } else if (playerSelectionFiltered === 'paper' && computerSelection === 'paper') {
-            console.log(`Paper vs Paper! It's a tie!`);
-            draw++;
-            return draw;
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const playerSelection = button.id;
+        const computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+        if (playerScore == 5) {
+            statusMsg.textContent = `player won! *mvp chants in the arena*`;
+        } else if (computerScore == 5) {
+            statusMsg.textContent = 'no chance. that computer is unbeatable!';
         } else {
-            console.log(`something is wrong.`);
-            }
+            statusMsg.textContent = 'game in progress';
         }
-    playRound(playerSelection, computerSelection);
-    console.log(`the result is: Player Wins ${playerScore}, Computer Wins ${computerScore}, Draws ${draw}`);
-}
-    if (playerScore > computerScore) {
-        console.log(`End of round 5: Victory!`);
-    } else if (computerScore > playerScore) {
-        console.log('End of round 5: The computer won.');
-    } else 
-    console.log('End of round 5: No winner - draw.');
-}
-
-game();
+    });
+});
